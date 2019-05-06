7f710db306b0673734eef8dccfcce4778e211cdf
Mai Thành Duy An <tiachop0102@gmail.com>
2019-05-06 22:34:11 +0700

    Register and  Login success.

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,11 @@ Route::get('/user', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
